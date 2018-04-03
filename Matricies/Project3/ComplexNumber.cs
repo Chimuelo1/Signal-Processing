c28461f375dcfbfb795ac71766f788cea0bbc22f
Tyler <tha7556@users.noreply.github.com>
2018-04-03 14:31:20 -0400

    Begun creating a ComplexNumber class

--- Matricies/Project3/ComplexNumber.cs
+++ Matricies/Project3/ComplexNumber.cs
@@ -1,8 +1,33 @@
-﻿using Matricies;
+﻿using System;
+
 namespace Project3 {
     class ComplexNumber {
+        double real, imag;
+        public ComplexNumber(double real, double imag) {
+            this.real = real;
+            this.imag = imag;
+        }
+        public double GetReal() {
+            return real;
+        }
+        public double GetImag() {
+            return imag;
+        }
+        public void SetReal(double real) {
+            this.real = real;
+        }
+        public void SetImag(double imag) {
+            this.imag = imag;
+        }
+        public override string ToString() {
+            return string.Format("({0} + {1}i)", real, imag);
+        }
+
         static void Main(string[] args) {
-            Matrix m;
+
+
+            Console.WriteLine("\n\nPress any key to close");
+            Console.ReadKey();
         }
     }
 }
